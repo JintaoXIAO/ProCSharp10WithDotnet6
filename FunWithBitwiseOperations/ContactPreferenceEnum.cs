08940b0c2646f8aa0fcb732231639f434e7443f4
jintao_xiao <xiaojintao@outlook.com>
2022-11-03 23:52:12 +0800

    update

---
+++ FunWithBitwiseOperations/ContactPreferenceEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace FunWithBitwiseOperations_;
+
+[Flags]
+public enum ContactPreferenceEnum
+{
+    None = 1,
+    Email = 2,
+    Phone = 3,
+    Text = 6
+}
---
+++ FunWithBitwiseOperations/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+using FunWithBitwiseOperations_;
+
+WriteLine("===== Fun with Bitwise Operations");
+ContactPreferenceEnum emailAndPhone = ContactPreferenceEnum.Email | ContactPreferenceEnum.Phone;
+WriteLine("None? {0}", (emailAndPhone | ContactPreferenceEnum.None) == emailAndPhone);
+WriteLine("None? {0}", (emailAndPhone | ContactPreferenceEnum.Phone) == emailAndPhone);
+WriteLine("None? {0}", (emailAndPhone | ContactPreferenceEnum.Text) == emailAndPhone);
